e25674a569d64ff1a7c2773d99e0af9620f6ed86
tomg <goresthomasj@johndeere.com>
2021-03-16 21:02:55 -0500

    update AircraftList.js with corrected references

--- src/components/AircraftList.js
+++ src/components/AircraftList.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 // looks at the list populate the a/c model
 // if click on one then update aircraft
 // update would be the edit
+
+
+
 const AircraftList = (props) => {
+    const foundAircraft = props.avmaints.find(aircraft=> {
+        return aircraft.id ==
+        props.match.params.id;
+    })
     return (
         <div>
-            {props.aircraft.map((aircraft, index) => (
-                <div key={index}>
-                    <h4>{aircraft.acmodel}</h4>
-                    <p>{aircraft.engmodel}</p>
-                    <Link to={`/aircraft/edit/${index}`}>EditAircraft</Link>
-                </div>
-             ))}
+            {/* {props.avmaints.find((avmaints, index) => (
+                <div key={index}> */}
+                    <h4>{foundAircraft.acmodel}</h4>
+                    <p>{foundAircraft.engmodel}</p>
+                    <Link to={`/aircraft/edit/${foundAircraft.id}`}>EditAircraft</Link>
+                {/* </div>
+             ))} */}
         </div>
     )
 }
